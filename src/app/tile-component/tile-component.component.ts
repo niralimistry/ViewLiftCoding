bdfc28a6c31c1b2a6af57d704e9d580f24ea2595
niralimistry <niralimistry999@gmail.com>
2018-01-26 11:28:35 -0500

    Second Commit

--- src/app/tile-component/tile-component.component.ts
+++ src/app/tile-component/tile-component.component.ts
@@ -1,17 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Ifilm} from '../film';
+
 
 @Component({
   selector: 'app-tile-component',
   templateUrl: './tile-component.component.html',
   styleUrls: ['./tile-component.component.css']
 })
-export class TileComponentComponent implements OnInit {
+export class TileComponentComponent {
 
   @Input() data: Ifilm;
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
